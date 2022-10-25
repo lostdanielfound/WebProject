2e6adf53af85f82e68e5632b401780c8bd947d79
Daniel Guzman <danielsonguzman2000@gmail.com>
2022-10-24 21:03:11 -0700

    Progress made with startup

--- WebProject/settings.py
+++ WebProject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'LifeFitness.apps.LifefitnessConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
